26240af64d71693a182a6a2746c49376aba8bb31
FGacheru <francisgacheru2001@gmail.com>
2020-10-02 11:13:08 +0300

    added arrays for Akan names

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var akanFemaleName=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var akanMaleName=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame];
+
+// var centuary=document.getElementById('centuary').
+// var year=document.getElementById('year')
+// var month=document.getElementById('month')
+// var day=document.getElementById('day')
