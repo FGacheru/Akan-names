fc29d4ee8d1d2ddbc2a9fe2e4d87afabdb093eab
FGacheru <francisgacheru2001@gmail.com>
2020-10-02 22:09:58 +0300

    modify idex.html

--- js/main.js
+++ js/main.js
@@ -15,10 +15,30 @@ function akanName() {
     var DD = day;
     var d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
 
-  if(gender==="Male"){
-    alert("Your Akan name" + akanMaleName[d])
-  
-  else {
-      alert("Your Akan name" + akanFemaleName[d])
-  }
+ if (gender==="Male") {
+     alert("Akan name is" + akanMaleName[d])
+ } 
+ if(gender==="Female") {
+     alert("Akan name is" + akanFemaleName[d])
+ }
+ if (DD=>0 || DD<32) {
+     alert("Valid date")
+ } else {
+     alert("Invalid date")
+ }
+ if (MM=>0 || MM<12 ) {
+     alert("Valid Month")
+ } else {
+     alert("Invalid Month")
+ }
+ if (CC>18 ||CC<=20) {
+     alert("Valid Centuary")
+ } else {
+    alert("Invalid Centuary") 
+ }
+ if (YY>00 || YY<=20) {
+     alert("Valid Year")
+ } else {
+    alert("Enter a valid Year") 
+ }
 }
