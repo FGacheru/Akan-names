0b0b6ed361dfce2040d9e7adbf3fb54a58327f84
FGacheru <francisgacheru2001@gmail.com>
2020-10-02 20:32:36 +0300

    modify js

--- js/main.js
+++ js/main.js
@@ -1,134 +1,24 @@
-var akanFemaleName=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var akanMaleName=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-function CC(){
-var centuary=perseInt(document.getElementById('centuary').value);
-}
-function yy(){
-var year=perseInt(document.getElementById('year').value);
-}
-function mm(){
-var month=perseInt(document.getElementById('month').value);
-}
-function dd(){
-var day=perseInt(document.getElementById('day').value);
-}
+function akanName() {
+    // event.preventDefault(); 
+    var centuary = parseInt(document.getElementById("centuary").value);
+    var year = parseInt(document.getElementById("year").value);
+    var month = parseInt(document.getElementById("month").value);
+    var day = parseInt(document.getElementById("day").value);
+    var gender = document.getElementById("gender").value;
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    )
+    var CC = centuary;
+    var YY = year;
+    var MM = month;
+    var DD = day;
+    var d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+
+  if(gender==="Male"){
+    alert("Your Akan name" + akanMaleName[d])
+  
+  else {
+      alert("Your Akan name" + akanFemaleName[d])
+  }
+}
