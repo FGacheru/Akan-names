26ecc88be042905fa93d67f357b2145ef060c774
FGacheru <francisgacheru2001@gmail.com>
2020-10-04 09:24:54 +0300

    add a background image

--- js/main.js
+++ js/main.js
@@ -2,43 +2,37 @@ var akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 function akanName() {
-    // event.preventDefault(); 
-    var centuary = parseInt(document.getElementById("centuary").value);
-    var year = parseInt(document.getElementById("year").value);
-    var month = parseInt(document.getElementById("month").value);
-    var day = parseInt(document.getElementById("day").value);
-    var gender = document.getElementById("gender").value;
+   var centuary = parseInt(document.getElementById("centuary").value);
+   var year = parseInt(document.getElementById("year").value);
+   var month = parseInt(document.getElementById("month").value);
+   var day = parseInt(document.getElementById("day").value);
+   var gender = document.getElementById("gender").value;
 
-    var CC = centuary;
-    var YY = year;
-    var MM = month;
-    var DD = day;
-    var d = parseInt(((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7
- if (gender==="Male") {
-     alert("Your Akan name is " + akanMaleName[4])
- } else
- if(gender==="Female") {
-     alert("Your Akan name is " + akanFemaleName[4])
- };
+   var CC = centuary;
+   var YY = year;
+   var MM = month;
+   var DD = day;
+   var d = parseInt(((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7
 
- if (DD=>0 || DD<32) {
-    //  alert("Valid date")
- } else {
-     alert("Invalid date")
- }
- if (MM=>0 || MM<12 ) {
-    //  alert("Valid Month")
- } else {
-     alert("Invalid Month")
- }
- if (CC>18 ||CC<=20) {
-    //  alert("Valid Centuary")
- } else {
-    alert("Invalid Centuary") 
- }
- if (YY>00 || YY<=20) {
-    //  alert("Valid Year")
- } else {
-    alert("Enter a valid Year") 
- }
+   if (CC < 18 || CC > 20) {
+      alert("Not a valid Centuary")
+   } else {
+
+      if (YY < 0 || 99 <= YY) {
+         alert("Invalid Year")
+      }else
+      if (MM <= 0 || MM > 12) {
+         alert("Enter a valid Month")
+      } else
+   if (DD <= 0 || DD > 31) {
+      alert("Invalid date")
+   } else
+   if (gender === "Male") {
+      alert("Your Akan name is " + akanMaleName[d])
+   }
+    if (gender === "Female"){
+      alert("Your Akan name is " + akanFemaleName[d])
+   }
 }
+}
+   
