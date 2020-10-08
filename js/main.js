a4a38d6d597f51b39742a0f0c3780897ea467c58
FGacheru <francisgacheru2001@gmail.com>
2020-10-08 23:25:46 +0300

    modified the project

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function akanName() {
    var gender = document.getElementById("gender").value;
 
    var CC = centuary;
-   var YY = year;
+   var YY = year;                                                                         
    var MM = month;
    var DD = day;
    var d = parseInt(((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7
@@ -18,7 +18,7 @@ function akanName() {
       alert("Not a valid Centuary")
    } else {
 
-      if (YY < 0 || 99 <= YY) {
+      if (YY < 0 || 100 <= YY) {
          alert("Invalid Year")
       }else
       if (MM <= 0 || MM > 12) {
